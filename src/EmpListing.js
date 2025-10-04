3a41d6e3db69f99af4ba1fb7484d980183ac957a
joo <joo@jooui-MacBookPro.local>
2025-10-05 06:15:03 +0900

    to 속성 사용

--- src/EmpListing.js
+++ src/EmpListing.js
@@ -6,12 +6,8 @@ const EmpListing = () => {
   const [empdata, setEmpdata] = useState(null);
   const navigate = useNavigate();
 
-  const LoadDetail = (id) => {
-    navigate("/employee/detail/" + id);
-  };
-  const LoadEdit = (id) => {
-    navigate("/employee/edit/" + id);
-  };
+  // LoadDetail 함수와 LoadEdit 함수를 제거했습니다.
+  
   const Removefunction = (id) => {
     if (window.confirm("Do you want to remove?")) {
       fetch("https://68e126f893207c4b47966580.mockapi.io/db/" + id, {
@@ -70,26 +66,25 @@ const EmpListing = () => {
                     <td>{item.email}</td>
                     <td>{item.phone}</td>
                     <td>
+                      {/* Edit 버튼: LoadEdit 함수 대신 to 속성으로 경로 직접 연결 */}
                       <Link
-                        onClick={() => {
-                          LoadEdit(item.id);
-                        }}
+                        to={`/employee/edit/${item.id}`}
                         className="btn btn-success"
                       >
                         Edit
                       </Link>
-                      <Link
+                      {/* Remove 버튼: a 태그와 onClick 사용 */}
+                      <a
                         onClick={() => {
                           Removefunction(item.id);
                         }}
                         className="btn btn-danger"
                       >
                         Remove
-                      </Link>
+                      </a>
+                      {/* Details 버튼: LoadDetail 함수 대신 to 속성으로 경로 직접 연결 */}
                       <Link
-                        onClick={() => {
-                          LoadDetail(item.id);
-                        }}
+                        to={`/employee/detail/${item.id}`}
                         className="btn btn-primary"
                       >
                         Details
